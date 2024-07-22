16420414ed3ad3ffea369feba41a55bb1ec7290e
Yash1756 <yashprajapati1756@gmail.com>
2024-07-22 13:53:38 +0530

    updated

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -148,7 +148,7 @@ const HomePage = () => {
                           disabled={item.isCompleted ? "true" : false}
                           style={item.isCompleted ? { opacity: "0.5" } : {}}
                         >
-                          Edit
+                          Editt
                         </button>
                       )}
                       <button
